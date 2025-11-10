851ad61ab76a77072f42533457fe6fa37d54c6e8
kamron-dev <k.kamron@yadnex.ru>
2025-11-10 13:33:02 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useReducer } from "react";
+import "./App.css";
+
+
+function actionsReducer(state, action) {
+  switch (action.type) {
+    case "fetch_started": {
+      return {
+        joke: {},
+        loading: true,
+        error: ""
+      }
+    };
+    case "fetch_success": {
+      return {
+        joke: action.joke,
+        loading: false,
+        error: ""
+      }
+    }
+    case "fetch_failed": {
+      return {
+        joke: {},
+        loading: false,
+        error: action.error
+      }
+    }  
+    default: return state;
+  }
+}
+
+export default function App() {
+  const [jokeState, dispatch] = useReducer(actionsReducer, {
+    joke: {},
+    loading: true,
+    error: ""
+  });
+
+
+  useEffect(() => {
+    dispatch({ type: "fetch_started" });
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then(res => res.json())
+      .then(data => {
+        dispatch({ type: "fetch_success", joke: data });
+      })
+      .catch(err => {
+      dispatch({type: "fetch_failed", error: err.message})
+    })
+
+  }, [])
+
+  if (jokeState.loading) return <h2>Loading...</h2>
+  if (jokeState.error) throw new Error(jokeState.error);
+
+  return (
+    <div>
+      <h2>
+        {jokeState.joke.setup} <br />
+        {jokeState.joke.punchline}
+      </h2>
+      <button onClick={() => window.location.reload(true)}>Get a new joke</button>
+    </div>
+  );
+};
+
+
+
+
+
